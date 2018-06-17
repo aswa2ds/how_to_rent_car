7b6a6789a07d34cc96b7b03aca5c74b4ba583d0d
edwardmartin927 <edwardmartin927@gmail.com>
2018-06-17 14:20:37 +0800

    update

--- how_to_rent_car/main.cpp
+++ how_to_rent_car/main.cpp
@@ -20,13 +20,22 @@ public:
 	int get_weight() { return weight; }
 };
 
+class cmp {
+public:
+	bool operator()(vertex &a, vertex &b) const
+	{
+		return a.get_weight() > b.get_weight();
+	}
+};
+
 class graph {
 private:
 	int scale;
 	color_type *color;
 	vector<vertex> *dependent;
-	priority_queue<vertex> heap;
-	int width;
+	priority_queue<vertex, vector<vertex>, cmp> heap;
+	int *width;
+	int wid;
 	int src;
 	int dst;
 public:
@@ -34,11 +43,14 @@ public:
 		scale = s; 
 		color = new color_type[s]; 
 		dependent = new vector<vertex>[s]; 
-		for (int i = 0; i < s; ++i)
+		width = new int[s];
+		for (int i = 0; i < s; ++i) {
 			color[i] = WHITE;
+			width[i] = 10000;
+		}
 		src = sr;
 		dst = ds;
-		width = 100;
+		wid = 100;
 	}
 	void add_edge(int, int, int);
 	void print();
@@ -53,8 +65,8 @@ int main() {
 	while (scanf_s("%d%d%d", &le, &u, &v) != EOF) {
 		G.add_edge(u, v, le);
 	}
-	G.print();
-	//G.prim(s);
+	//G.print();
+	G.prim(s);
 	return 0;
 }
 
@@ -74,3 +86,33 @@ void graph::print() {
 	}
 }
 
+void graph::prim(int u) {
+	width[u] = 0;
+	for (unsigned i = 0; i < dependent[u].size(); ++i) {
+		int v = dependent[u][i].get_num();
+		int w = dependent[u][i].get_weight();
+		int max_width = (w > width[u]) ? w : width[u];
+		//cout << max_width << endl;
+		width[v] = (max_width < width[v]) ? max_width : width[v];
+		heap.push(dependent[u][i]);
+	}
+	while (!heap.empty()) {
+		vertex tmp = heap.top();
+		heap.pop();
+		int m = tmp.get_num();
+		for (unsigned i = 0; i < dependent[m].size(); ++i) {
+			int n = dependent[m][i].get_num();
+			int w = dependent[m][i].get_weight();
+			int max_width = (w > width[m]) ?  w : width[m];
+			if (width[n] > max_width) {
+				width[n] = max_width;
+				heap.push(dependent[m][i]);
+			}
+		}
+	}
+	/*for (int i = 0; i < scale; ++i)
+		cout << width[i] << " ";
+	cout << endl;*/
+	cout << "I have read the rules about plagiarism punishment" << endl;
+	cout << width[dst] << endl;
+}
